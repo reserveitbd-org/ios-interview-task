89454ed69c9b700bbe2fdddb8f77cb2fcbfa2d9d
csemozid <csemozid@gmail.com>
2023-01-10 23:43:22 +0600

    Added project skeleton

---
+++ reserveit/reserveitApp.swift
@@ -0,0 +1,17 @@
+//
+//  reserveitApp.swift
+//  reserveit
+//
+//  Created by Md. Abdul Mozid on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct reserveitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
